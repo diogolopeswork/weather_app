3cd15e420650358b0816f166578f7488ba61b253
Diogo Lopes <deosl98@gmail.com>
2021-11-23 23:07:26 +0000

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ $(function () {
 $(document).on('keypress', function(e) {
     let keyPressed = e.which
     let locInput = $('#location-input').val()
-    // changeLocation(keyPressed, locInput)
+    changeLocation(keyPressed, locInput)
 })
 
 function getWeatherData() {
@@ -16,90 +16,6 @@ function getWeatherData() {
 
 function displayWeather(data) {
     console.log('weather', data)
-    // let wCondition = data.current.condition.text
-
-    /* switch (wCondition) {
-        case "Clear":
-            $('.condition-text').text(wCondition)
-            break
-        case "Partly cloudy":
-            $('.condition-text').text(wCondition)
-            break
-        case "Cloudy":
-            $('.condition-text').text(wCondition)
-            break
-        case "Sunny":
-            $('.condition-text').text(wCondition)
-            break
-        case "Fog":
-            $('.condition-text').text(wCondition)
-            break
-        case "Overcast":
-            $('.condition-text').text(wCondition)
-            break
-        case "Mist":
-            $('.condition-text').text(wCondition)
-            break
-        case "Patchy light rain":
-            $('.condition-text').text(wCondition)
-            break
-        case "Light rain":
-            $('.condition-text').text(wCondition)
-            break
-        case "Moderate rain at times":
-            $('.condition-text').text(wCondition)
-            break
-        case "Moderate rain":
-            $('.condition-text').text(wCondition)
-            break
-        case "Heavy rain at times":
-            $('.condition-text').text(wCondition)
-            break
-        case "Heavy rain":
-            $('.condition-text').text(wCondition)
-            break
-        case "Patchy light snow":
-            $('.condition-text').text(wCondition)
-            break
-        case "Light snow":
-            $('.condition-text').text(wCondition)
-            break
-        case "Patchy moderate snow":
-            $('.condition-text').text(wCondition)
-            break
-        case "Moderate snow":
-            $('.condition-text').text(wCondition)
-            break
-        case "Patchy heavy snow":
-            $('.condition-text').text(wCondition)
-            break
-        case "Heavy snow":
-            $('.condition-text').text(wCondition)
-            break
-        case "Light rain shower":
-            $('.condition-text').text(wCondition)
-            break
-        case "Moderate or heavy rain shower":
-            $('.condition-text').text(wCondition)
-            break
-        case "Torrential rain shower":
-            $('.condition-text').text(wCondition)
-            break
-        case "Light snow showers":
-            $('.condition-text').text(wCondition)
-            break
-        case "Moderate or heavy snow showers":
-            $('.condition-text').text(wCondition)
-            break
-        case "Patchy light rain with thunder":
-            $('.condition-text').text(wCondition)
-            break
-        case "Moderate or heavy rain with thunder":
-            $('.condition-text').text(wCondition)
-            break
-        default:
-            $('.condition-text').text('No idea of the weather, take a look outside')
-    } */
 
     for(let wCat of Object.keys(data.forecast.forecastday)) {
         // console.log('data.forecast =>', data.forecast.forecastday[wCat])
@@ -116,16 +32,16 @@ function displayWeather(data) {
             let hourCondText = hour.condition.text
             let hourIcon = hour.condition.icon
             let elem = $(`
-                <div class="slick-slide glass-container forecast-card">
-                    <span class="forecast-card-hour">${wHour.time.slice(10)}</span>
+                <div class="slick-slide forecast-card">
+                    <span class="details-text">${wHour.time.slice(10)}</span>
                     <img class="forecast-card-icon" src="${hourIcon}">
-                    <span class="forecast-temp">${hourCondText}</span>
-                    <div class="forecast-temp-container">
-                        <span class="forecast-temp">${temp + 'º'}</span>
+                    <span class="details-text">${hourCondText}</span>
+                    <div class="details-text-container">
+                        <span class="details-text">${temp + 'º'}</span>
                     </div>
-                    <div class="rain-percent-container">
+                    <div class="details-text-container">
                         <img src="./assets/img/weather_icons/day/302.png"> 
-                        <span class="rain-percent">${rainPercent + '%'}</span>
+                        <span class="details-text">${rainPercent + '%'}</span>
                     </div>
                 </div>
             `)
@@ -136,11 +52,19 @@ function displayWeather(data) {
 
     let currWeatherElem = $(`
         <div class="row">
-            <div class="col-6">
+            <div class="col-6 d-flex justify-content-start">
                 <h4 class="city-text">Ílhavo</h4>
             </div>
-            <div class="col-6">
-                . . .
+        </div>
+        <div class="row mt-3">
+            <div class="col-6 d-flex flex-column text-start">
+                <span class="curr-temp-text">${data.current.temp_c + 'º'}</span>
+                <span class="details-text">${data.current.condition.text}</span>
+                <span class="details-text">${'Wind' + ' ' + data.current.wind_kph + ' ' + 'km/h'}</span>
+                <span class="details-text">${'Humidity' + ' ' + data.current.humidity + '%'}</span>
+            </div>
+            <div class="col-6 m-auto">
+                <img class="current-weather-img" src="${data.current.condition.icon}">
             </div>
         </div>
     `)
@@ -180,6 +104,7 @@ function setBackground(time) {
     if(time > 7 && time < 19) {
         $('section.hero').css('background-image', 'url(./assets/img/day.jpg')
         $('body').css('color', 'black')
+        $('a').css('color', 'black')
         $('.glass-container').css('background', 'linear-gradient(to right bottom, var(--lighterglass), var(--lightglass))')
     }
     /* let wCondition = weather.current.condition.text
@@ -227,5 +152,4 @@ function changeLocation(key, val) {
 /* TODO:
     - Descomentar call da function changeLocation
     - Descomentar input no index.html
-
 */
