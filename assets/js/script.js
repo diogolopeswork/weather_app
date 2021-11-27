86a79819d3bad0fffbca45b9f42a6b26775e7ab9
Diogo Lopes <deosl98@gmail.com>
2021-11-27 21:02:46 +0000

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,7 @@
-$(function () {
+/* $(function () {
     getWeatherData()
     getDate()
 })
-
-function getWeatherData() {
-    $.get('https://api.weatherapi.com/v1/forecast.json?key=6a0c143f81ec4ec7a5b173316212111&q=Ilhavo&days=1&aqi=no&alerts=no')
-        .done(response => displayWeather(response))
-}
 
 function displayWeather(data) {
     for (let wCat of Object.keys(data.forecast.forecastday)) {
@@ -60,34 +55,6 @@ function displayWeather(data) {
     $('.current-weather-container').append(currWeatherElem)
 }
 
-function getDate() {
-    const date = new Date()
-    const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-    const year = date.getFullYear()
-    const month = months[date.getMonth()]
-    const day = date.getDate()
-    const weekDay = weekDays[date.getDay()]
-    $('.date-text').text(weekDay + ' ' + '|' + ' ' + day + ' ' + month + ' ' + year)
-
-    setInterval(() => {
-        getTime()
-    }, 1000)
-}
-
-function getTime() {
-    const date = new Date()
-    const hour = date.getHours()
-    const min = date.getMinutes()
-    const sec = date.getSeconds()
-    $('.time-text').text(formatTime(hour) + ':' + formatTime(min) + ':' + formatTime(sec))
-    setBackground(date)
-}
-
-function formatTime(time) {
-    return time < 10 ? (`0${time}`) : time
-}
-
 function setBackground(time) {
     if (time > 7 && time < 19) {
         $('section.hero').css('background-image', 'url(./assets/img/day.jpg')
@@ -96,8 +63,19 @@ function setBackground(time) {
         $('.glass-container').css('background', 'linear-gradient(to right bottom, var(--lighterglass), var(--lightglass))')
     }
 }
+*/
 
-function initSlick() {
+$(function () {
+    getWeatherData()
+    getDate()
+})
+
+function getWeatherData() {
+    $.get('https://api.weatherapi.com/v1/forecast.json?key=6a0c143f81ec4ec7a5b173316212111&q=Ilhavo&days=1&aqi=no&alerts=no')
+        .done(response => displayWeather(response))
+}
+
+/* function initSlick() {
     $('.slick-slider').on('init', function (event, slick) {
         $('.slick-slider').removeClass('overflow');
     });
@@ -116,6 +94,84 @@ function initSlick() {
             }
         }]
     })
+} */
+
+function getDate() {
+    const date = new Date()
+    const weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    const year = date.getFullYear()
+    const month = months[date.getMonth()]
+    const day = date.getDate()
+    const weekDay = weekDays[date.getDay()]
+    $('.date-text').text(weekDay + ' ' + day + ' ' + month + ' ' + year)
+
+    setInterval(() => {
+        getTime()
+    }, 1000)
+}
+
+function getTime() {
+    const date = new Date()
+    const hour = date.getHours()
+    const min = date.getMinutes()
+    const sec = date.getSeconds()
+    $('.time-text').text(formatTime(hour) + ':' + formatTime(min) + ':' + formatTime(sec))
+    // setBackground(date)
+}
+
+function displayWeather(data) {
+    console.log(data)
+    /* for (let wCat of Object.keys(data.forecast.forecastday)) {
+        let container = $(`
+            <div class="slick-slider forecast-slider"></div>
+        `)
+        $('.forecast-container').append(container)
+        for (let wHour of data.forecast.forecastday[wCat].hour) {
+            let hour = wHour
+            let temp = hour.temp_c
+            let rainPercent = hour.chance_of_rain
+            let hourCondText = hour.condition.text
+            let hourIcon = hour.condition.icon
+            let elem = $(`
+                <div class="slick-slide forecast-card">
+                    <span class="details-text">${wHour.time.slice(10)}</span>
+                    <img class="forecast-card-icon" src="${hourIcon}">
+                    <span class="details-text">${hourCondText}</span>
+                    <div class="details-text-container">
+                        <span class="details-text">${formatUnits(temp) + 'º'}</span>
+                    </div>
+                    <div class="details-text-container">
+                        <img src="./assets/img/weather_icons/day/302.png"> 
+                        <span class="details-text">${formatUnits(rainPercent) + '%'}</span>
+                    </div>
+                </div>
+            `)
+            $('.forecast-slider').append(elem)
+        }
+        initSlick()
+    } */
+
+    let currWeatherElem = $(`
+        <div class="curr-temp-container">
+            <span class="temp-text">${formatUnits(data.current.temp_c) + 'º'}</span>
+            <span class="city-text fw-lighter">${data.location.name}</span>
+        </div>
+    `)
+    let currWeather = $(`
+        <div class="curr-condition-container">
+            <span class="fw-lighter">${data.current.condition.text}</span>
+            <img class="current-weather-img" src="${data.current.condition.icon}">
+        </div>
+    `)
+    $('.hero-left').append(currWeatherElem)
+    $('.hour-n-date').append(currWeather)
+}
+
+
+
+function formatTime(time) {
+    return time < 10 ? (`0${time}`) : time
 }
 
 function formatUnits(unit) {
